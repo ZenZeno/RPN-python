2d8f81bef79396435d704fbcc31eeda89a9c4637
ZenZeno <zacherycharlesk@gmail.com>
2018-03-19 16:29:24 -0400

    Organized operator logic into functions and added error checking

--- reverse_polish_notation.py
+++ reverse_polish_notation.py
@@ -1,3 +1,24 @@
+def binary_operator(x, y, operator):
+
+    if operator == '*':
+        return x * y
+    elif operator == '+':
+        return x + y
+    elif operator == '-':
+        return x - y
+    elif operator == '/' and y != 0:
+        return x / y
+    else:
+        #operator was not a valid symbol, so the expression is poorly formatted
+        return 'ERR'
+
+def unary_operator(x, operator):
+
+    if operator == '!':
+        return -x
+    else:
+        return 'ERR'
+
 calc_string = input("Please enter a RPN expression to evaluate, or q to quit: ")
 
 while calc_string != 'q':
@@ -10,32 +31,27 @@ while calc_string != 'q':
 
         next_token = calc_list.pop()
         
-        # if the next token is not a digit or a unary operand, set operand = 'ERR'. Then, the next if statement
+        # if the next token is not a digit or a unary operator, set operator = 'ERR'. Then, the next if statement
         # will cause an  error message to be printed
         if next_token.isdigit():
-             y =float(next_token)
-             operand = calc_list.pop()
-        elif next_token == '!':
-            operand = next_token 
+            y =float(next_token)
+            try:
+                operator = calc_list.pop()
+                result = binary_operator(x, y, operator)
+            except:
+                result = 'ERR'
         else:
-            operand = 'ERR'
-
-        if operand == '*':
-            calc_list.append(x * y)
-        elif operand == '+':
-            calc_list.append(x + y)
-        elif operand == '-':
-            calc_list.append(x - y)
-        elif operand == '/' and y != 0:
-            calc_list.append(x / y)
-        elif operand == '/' and y == 0:
-            calc_list = ['Error: Division by 0']
-        elif operand == '!':
-            calc_list.append(-x)
+            operator = next_token
+            result = unary_operator(x, operator)
+            
+        if result == 'ERR':
+            print("Error: Improperly formatted expression")
         else:
-            #operand was not a valid symbol, so the expression is poorly formatted
-            calc_list = ['Error: Imroperly formatted expression']
+            calc_list.append(result)
+    try:
+        print(calc_list[0])
+    except:
+        pass #Error message already printed, we just want to supress Index Out of Bounds error
 
-    print(calc_list[0])
     calc_string = input("Please enter a RPN expression to evaluate, or q to quit: ")
 
